0e04dfba4b204a17a2a6aa64a07aff3399405054
Joel Wallis Jucá <joelwallis@gmail.com>
2017-08-23 15:18:25 -0300

    Commander FTW
    https://github.com/tj/commander.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+const program = require('commander')
+const package = require('../package.json')
+
+program
+  .version(package.version)
+  .parse(process.argv)
+
 console.log('Hello fom Dsafio!')
