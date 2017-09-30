b1354270c2a6e89794d7f73453c505b5707e9f03
Joel Wallis Jucá <joelwallis@gmail.com>
2017-09-30 13:35:53 -0300

    lib/registry tests now using lib/fs-as-promise
    See #24

--- test/lib/registry.test.js
+++ test/lib/registry.test.js
@@ -1,8 +1,8 @@
-const fs       = require('fs')
 const chai     = require('chai')
 const sinon    = require('sinon')
 const util     = require('util')
 // const config   = require('../../lib/config')
+const fs       = require('../../lib/fs-as-promise')
 const registry = require('../../lib/registry')
 
 const expect = chai.expect
@@ -17,6 +17,12 @@ describe('lib/registry', function () {
   it('is an object', () => expect(registry).to.be.an('object'))
 
   describe('get()', function () {
+
+    beforeEach(() => sinon.stub(fs, 'readFile').returns(Promise.resolve(JSON.stringify({
+      foo: 'foo',
+      bar: 'bar'
+    }))))
+    afterEach(() => fs.readFile.restore())
 
     it('is a function', () => expect(registry.get).to.be.a('function'))
 
