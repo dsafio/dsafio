5adc26ff8e2fa5e34aad4b36b02948a0288d8648
Joel Wallis Jucá <joelwallis@gmail.com>
2017-09-30 22:05:28 -0300

    Rejected promises instead of direct thrown errors
    - lib/config now returns rejected promises instead of throwing errors
    - lib/registry too 👆
    - lib/config and lib/registry tests updated, now supporting promises
    - chai-as-promised was used to make it possible 👆
    - No more weird env variables during tests. IO ops are now mocked 😎
    - A big commit I know, but no patience to split it now (it's 10:50 PM) 😴
    
    Closes  #6

--- test/lib/registry.test.js
+++ test/lib/registry.test.js
@@ -1,16 +1,9 @@
 const chai     = require('chai')
 const sinon    = require('sinon')
-const util     = require('util')
-// const config   = require('../../lib/config')
 const fs       = require('../../lib/fs-as-promise')
 const registry = require('../../lib/registry')
 
 const expect = chai.expect
-
-if (!process.env.DSAFIO_CACHE_FILE) {
-  console.log('Please provide an environment variable DSAFIO_CACHE_FILE')
-  process.exit(1)
-}
 
 describe('lib/registry', function () {
 
@@ -18,26 +11,45 @@ describe('lib/registry', function () {
 
   describe('get()', function () {
 
-    beforeEach(() => sinon.stub(fs, 'readFile').returns(Promise.resolve(JSON.stringify({
-      foo: 'foo',
-      bar: 'bar'
-    }))))
-    afterEach(() => fs.readFile.restore())
+    beforeEach(() => {
+      sinon.stub(fs, 'readFile').returns(Promise.resolve(JSON.stringify({
+        foo: 'foo',
+        bar: 'bar'
+      })))
+      sinon.stub(fs, 'writeFile')
+    })
+    afterEach(() => {
+      fs.readFile.restore()
+      fs.writeFile.restore()
+    })
 
     it('is a function', () => expect(registry.get).to.be.a('function'))
 
-    it('accepts an array as argument', function () {
-      expect(() => registry.get()).to.not.throw()
-      expect(() => registry.get(['foo', 'bar'])).to.not.throw()
+    it('accepts an array as argument', () => {
+      return Promise.all([
+        expect(registry.get()).to.be.fulfilled,
+        expect(registry.get(['foo', 'bar'])).to.be.fulfilled
+      ])
+    })
 
-      ;['foo', true, 123, null, [], {}].forEach(value => {
-        expect(() => registry.get(value)).to.throw()
-      })
+    it('rejects when passed argument other than array', () => {
+      return Promise.all(['foo', true, 123, null, [], {}].map(value => {
+        return expect(registry.get(value)).to.be.rejected
+      }))
     })
 
     it('returns a promise', function () {
-      expect(registry.get()).to.be.a('promise')
-      expect(registry.get(['foo', 'bar'])).to.be.a('promise')
+      const promises = []
+
+      expect((() => {
+        promises.push(registry.get())
+        return promises.slice(-1)[0]
+      })()).to.be.a('promise')
+
+      expect((() => {
+        promises.push(registry.get(['foo', 'bar']))
+        return promises.slice(-1)[0]
+      })()).to.be.a('promise')
     })
 
   })
