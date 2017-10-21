bf44f20251262a638581fe39f15703fa59b199f8
Joel Wallis Jucá <joelwallis@gmail.com>
2017-10-21 01:39:22 -0300

    debug() in bin/dsafio-config.js

--- bin/dsafio-config.js
+++ bin/dsafio-config.js
@@ -6,11 +6,15 @@ const pkg     = require('../package.json')
 const config  = require('../lib/config')
 /* eslint-enable no-multi-spaces */
 
+const debug = require('debug')('dsafio/cli/config')
+debug('running dsafio-config')
+
 program
   .version(pkg.version)
   .parse(process.argv)
 
 const operation = program.args.shift()
+debug(`operation: ${operation}`)
 
 if (!['get', 'set'].includes(operation)) {
   console.error('Invalid config operation. See --help for more info.')
@@ -22,14 +26,22 @@ switch (operation) {
     var args
 
     if (program.args.length) args = program.args
+    debug(`arguments: ${args}`)
 
+    debug('loading configuration')
     config.get(args)
-      .then(entries => Object.keys(entries)
-        .map(key => `${key}=${entries[key]}`)
-        .join('\n'))
+      .then(entries => {
+        debug('configuration loaded:', entries)
+
+        return Object.keys(entries)
+          .map(key => `${key}=${entries[key]}`)
+          .join('\n')
+      })
       .then(console.info.bind(console))
       .catch(error => {
-        ['EACCES', 'ENOENT'].includes(error.code)
+        debug(error)
+
+        ;['EACCES', 'ENOENT'].includes(error.code)
           ? console.error('Inaccessible or inexistent configuration file')
           : console.error('Something went wrong while reading configuration file')
         process.exit(1)
@@ -45,8 +57,14 @@ switch (operation) {
 
         return { [program.args[0]]: program.args[1] }
       })
-      .then(config.set)
+      .then(entries => {
+        debug('setting configuration:', entries)
+
+        config.set(entries)
+      })
       .catch(error => {
+        debug(error)
+
         ['EACCES', 'ENOENT'].includes(error.code)
           ? console.error('Inaccessible or inexistent configuration file')
           : console.error('Something went wrong while reading configuration file')
