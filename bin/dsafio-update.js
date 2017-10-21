f4dd16d2465a35b2152a82381d05def01bf68cbd
Joel Wallis Jucá <joelwallis@gmail.com>
2017-10-21 02:40:52 -0300

    debug() in bin/dsafio-update.js

--- bin/dsafio-update.js
+++ bin/dsafio-update.js
@@ -6,12 +6,18 @@ const pkg      = require('../package.json')
 const registry = require('../lib/registry')
 /* eslint-enable no-multi-spaces */
 
+const debug = require('debug')('dsafio/cli/update')
+debug('running dsafio-update')
+
+
 program
   .version(pkg.version)
   .parse(process.argv)
 
 registry.update()
   .catch(() => {
+    debug(error)
+
     console.error('Something went wrong while updating the registry')
     process.exit(1)
   })
